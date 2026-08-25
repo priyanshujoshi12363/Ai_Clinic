fcb6b018c9fed55664803843ba88927f0ecdf295
Priyanshu Joshi <joshipriyanshu575@gmail.com>
2026-08-26 00:57:39 +0530

    first

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express, { response } from "express"
+const app = express()
+const port = 4000;
+
+app.get("/" , ( req , res)=>{
+    res.send("hello world")
+})
+
+app.listen(port , ()=>{
+    console.log(`server is running on ${port}`)
+})
